24cb74296630e0cab06d13b525c3941b60061580
Himanshu Kumar <77279763+himanshu32288@users.noreply.github.com>
2021-12-09 14:24:56 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,12 +5,10 @@ const Navbar = props => {
         <nav className="navbar navbar-expand-lg navbar navbar-dark bg-dark">
             <div className="container-fluid">
                 <a className="navbar-brand" href="/">Nepilson</a>
-               <button className="navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#collapsable" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"/>
-               </button>
-                <div className="collapse navbar-collapse" id="collapsable">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div className="navbar-nav">
                         <a className="nav-link active" aria-current="page" href="/">NFA TO DFA</a>
                     </div>
